8dc7052e02b5789cc635c6e0939a3377b2a4e147
Bruno Furquim <furquim001@gmail.com>
2025-02-18 20:35:23 -0300

    Projeto Conta Banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -2,7 +2,8 @@ import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.println("Por favor, digite o seu nome: ");
@@ -19,4 +20,5 @@ public class ContaTerminal {
 
         System.out.println("Olá "+ nome + ", obrigado por criar uma conta em nosso banco! Sua agência é: " + agencia + ", conta: " + numero + " e seu saldo: " + saldo + " já está disponível para saque!");
     }
+
 }
